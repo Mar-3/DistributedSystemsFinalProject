6c0d52db93e891a1162b61320d87ccb38718d404
Mar-3 <=>
2024-04-19 15:05:09 +0300

    huuhaa endpoints for testing

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,16 @@ app = FastAPI()
 class TestItem(BaseModel):
     name: str
 
+class MemoItem(BaseModel):
+    objectId: int
+    object:str
+    x:int
+    y:int
+    text:str
+    bgcolor:str
+
+items = []
+
 @app.get("/")
 async def test():
     return {"Hello" : "World"}
@@ -14,3 +24,22 @@ async def test():
 @app.post("/echo")
 async def echo(item: TestItem):
     return {"got": item.name}
+
+
+
+# Testing posts and gets with frontend
+# list used for storage for now :D 
+# routes are bad and will be made proper at a later date
+@app.post("/objects/add/memo")
+async def addMemo(MemoItem: MemoItem):
+    try: {
+        items.append(MemoItem)
+
+    }
+    except:
+        return {"success": False}
+    return {"success": True}
+
+@app.get("/objects/get/memo/all")
+async def getMemo():
+    return {"items": items}
